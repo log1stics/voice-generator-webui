75189de96682cd70d128baed19ddd89262d0c0b0
log1stics <you@example.com>
2023-04-18 14:39:35 +0900

    license

--- webui.py
+++ webui.py
@@ -8,8 +8,15 @@ from ui import themes, generate, vc_batch, train, settings
 
 seafoam = themes.Seafoam()
 
+top = '''
+  <div align="center">
+  <a href="https://twitter.com/q107z"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Logo_of_Twitter%2C_Inc..svg/292px-Logo_of_Twitter%2C_Inc..svg.png" width="30"></a>
+  </div>
+  <br>
+'''
 
 with gr.Blocks(theme=seafoam) as app:
+    gr.Markdown(top)
     with gr.Tabs():
         generate.ui()
         vc_batch.ui()
