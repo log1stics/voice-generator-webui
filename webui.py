52270460bebacd4a004ab33c96cd13721e1f9d8a
log1stics <you@example.com>
2023-05-23 17:32:41 +0900

    modify webui.py

--- webui.py
+++ webui.py
@@ -7,18 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--colab", action="store_true", help="Launch in colab")
 iscolab = parser.parse_args().colab
 
-
-seafoam = themes.Seafoam()
-
-top = '''
-  <div align="center">
-  <a href="https://twitter.com/q107z"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/292px-Logo_of_Twitter.svg.png" width="30"></a>
-  </div>
-  <br>
-'''
-
-with gr.Blocks(theme=seafoam) as app:
-    gr.Markdown(top)
+with gr.Blocks(themes.Seafoam()) as app:
     with gr.Tabs():
         generate.ui()
         generate_with_espnet.ui()
